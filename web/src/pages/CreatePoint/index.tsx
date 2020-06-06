e7d59d03bf9a5f4d7e0b8b4c96f26f1faac66496
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-06-06 18:54:45 -0300

    feat: add search for states and city

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,10 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState, ChangeEvent } from 'react'
 import './styles.css'
 import logo from '../../assets/logo.svg'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api'
+import axios from 'axios'
+interface Item { 
+    id: number
+    name: string
+    image_url: string
+}
 
-const Home = () => { 
+interface IBGEUFResponse { 
+    sigla: string
+}
+
+interface IBGECityResponse { 
+    nome: string
+}
+const CreatePoint = () => { 
+    const [items, setItems] = useState<Item[]>([])
+    const [ufs, setUfs] = useState<string[]>([])
+    const [cities, setCities] = useState<string[]>([])
+
+    const [selectedUf, setSelectedUf] = useState("0")
+    const [selectedCity, setSelectedCity] = useState("0")
+
+
+    useEffect(()=>{
+        api.get('items').then((response)=> { 
+            setItems(response.data)
+        }).catch((error)=> { 
+            console.log(error)
+        })
+    },[]);
+
+    useEffect(()=>{
+        api.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+        .then((response)=> { 
+            const ufInitials = response.data.map(uf => uf.sigla)
+            setUfs(ufInitials)
+        }).catch((error)=> { 
+            console.log(error)
+        })
+    },[]);
+
+    useEffect(()=>{
+        if (selectedUf == '0') { 
+            return
+        }
+        api.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+        .then((response)=> { 
+            const cityNames = response.data.map(city => city.nome)
+            setCities(cityNames)
+        }).catch((error)=> { 
+            console.log(error)
+        })
+    },[selectedUf]);
+
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) { 
+        const uf = event.target.value
+        setSelectedUf(uf)
+    }
+
+    function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) { 
+        const city = event.target.value
+        setSelectedCity(city)
+    }
+
+
     return (
         <div id="page-create-point">
             <header>
@@ -48,19 +113,40 @@ const Home = () => {
                     <legend>
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
+                        
                     </legend>
+                    <Map center={[-23.5553977,-46.7381793]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-23.5553977,-46.7381793]}/>
+                    </Map>
+              
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
-                            <select name="uf" id="uf">
+                            <select name="uf" 
+                                    id="uf"
+                                    value={selectedUf}
+                                    onChange={handleSelectUf}>
                                <option value="0">Selecione uma UF</option> 
+                               {ufs.map(uf => (
+                                  <option key={uf} value={uf}>{uf}</option>  
+                               ))}
                             </select>
                         </div>
 
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city">
+                            <select name="city" 
+                                    id="city"
+                                    value={selectedCity}
+                                    onChange={handleSelectCity}>
                                <option value="0">Selecione uma Cidade</option> 
+                               {cities.map(city => (
+                                  <option key={city} value={city}>{city}</option>  
+                               ))}
                             </select>
                         </div>
                     </div>
@@ -72,30 +158,12 @@ const Home = () => {
                         <span>Selecione um ou mais itens abaixo</span>
                     </legend>
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>óleo</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>óleo</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>óleo</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>óleo</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>óleo</span>
-                        </li>
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>óleo</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                              <img src={item.image_url} alt={item.name}/>
+                              <span>{item.name}</span>
+                             </li>
+                        ))}
                     </ul>
                 </fieldset>
                 <button type="submit">
@@ -106,4 +174,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default CreatePoint
