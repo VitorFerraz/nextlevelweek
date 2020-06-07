defdae9bf37008ffbef2ccaa1142444001b6b372
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-06-07 14:54:42 -0300

    feat: add mail and whatsapp integration,  featch detail point and items

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -15,6 +15,7 @@ class PointsController {
                                 .where('uf', String(uf))
                                 .distinct()
                                 .select('points.*')
+        console.log(points)
         return response.json(points)
 
 
