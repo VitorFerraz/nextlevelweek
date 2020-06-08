a0ed81b4cf9aeddc4b2328499696ce6350fc4b5a
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-06-08 11:06:48 -0300

    feat: add form validation

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -12,6 +12,7 @@
 
 
 import express, { request } from "express";
+import {celebrate, Joi} from 'celebrate'
 import multer from 'multer'
 import multerConfig from './config/multer'
 import PointsController from './controllers/PointsController'
@@ -26,7 +27,19 @@ const itemsController = new ItemsController()
 
 routes.get('/items', itemsController.index)
 
-routes.post('/points',upload.single('image'), pointsController.create)
+routes.post('/points',upload.single('image'), celebrate({
+    body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required()
+    })
+}),pointsController.create)
+
 routes.get('/points/:id', pointsController.show)
 routes.get('/points', pointsController.index)
 
